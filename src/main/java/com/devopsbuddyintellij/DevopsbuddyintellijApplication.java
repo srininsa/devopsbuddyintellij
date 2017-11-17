43654ece0d4ff3ac85ad2aa80b7bbf976d26bef8
Srinivasan Natanmai <srini.nsa@gmail.com>
2017-11-17 16:06:53 +0000

    First checkin of HelloWorld

---
+++ src/main/java/com/devopsbuddyintellij/DevopsbuddyintellijApplication.java
@@ -0,0 +1,12 @@
+package com.devopsbuddyintellij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsbuddyintellijApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsbuddyintellijApplication.class, args);
+	}
+}
---
+++ src/test/java/com/devopsbuddyintellij/DevopsbuddyintellijApplicationTests.java
@@ -0,0 +1,16 @@
+package com.devopsbuddyintellij;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DevopsbuddyintellijApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
