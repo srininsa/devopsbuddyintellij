4d9fa04d30ccaf89f412fb6c8fb2d2b87fb8a8cc
Srinivasan Natanmai <srini.nsa@gmail.com>
2017-11-17 16:18:51 +0000

    Initial commit of HelloWorldController

---
+++ src/main/java/com/devopsbuddyintellij/controllers/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.devopsbuddyintellij.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "index";
+    }
+}
